3d0f28caff267aaa60356dd6ab465bbc7c711998
Rikesh Lal Shrestha <rikesh.shrestha@citytech.global>
2020-01-18 06:22:27 +0545

    setting up

--- config/serverConfig.js
+++ config/serverConfig.js
@@ -1,8 +1,26 @@
+import http from "http";
+
 const config ={
     port : process.env.APP_SERVER_PORT || "3002",
     headers:{ 'content-type': 'application/json' },
     mongoURL: process.env.MONGO_URL || 'mongodb://localhost:27017/',
     dbName: 'blog-post',
+};
+
+export const openServer = (app) => {
+    const server = http.createServer(app);
+
+    server
+        .listen(config.port, () => {
+            console.log(`Server running at port : http://localhost:${server.address().port}/`);
+        })
+        .on('error', (err) => {
+            {
+                if (err.code === 'EADDRINUSE') {
+                    openServer(app,Number(config.port) + 1);
+                }
+            }
+        })
 };
 
 export default config;
--- index.js
+++ index.js
@@ -1,19 +1,14 @@
 /**
  * Entry Script
  */
-
-import http from 'http';
 import express from 'express';
 import logger from 'morgan';
 import cors from 'cors';
 import path from 'path';
-
 import dotenv from 'dotenv';
 const dotEnvConfig = dotenv.config();
 
-import config from './config/serverConfig.js'
-
-const port = process.env.APP_SERVER_PORT || "3002";
+import {openServer} from './config/serverConfig.js'
 
 const app = express();
 
@@ -23,8 +18,4 @@ app.use(logger('dev'));
 app.disable('etag');
 app.use(express.static(path.resolve(__dirname, './client/build')));
 
-const server = http.createServer(app);
-
-server.listen(port, () => {
-    console.log(`Server running at port : http://localhost:${server.address().port}/`);
-});
+openServer(app);
