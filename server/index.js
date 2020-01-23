15ae06fb6e4566070060af2a743920c3eb433acf
Rikesh Lal Shrestha <rikesh.shrestha@citytech.global>
2020-01-23 16:56:53 +0545

    0..0

--- server/index.js
+++ server/index.js
@@ -16,6 +16,10 @@ app.get("*", (req, res) => {
 
 const port = process.env.PORT || "3000";
 
-app.listen(port,()=>{
-    console.log(`Server running at port :${port}`);
-})
+
+module.exports = start = () =>{
+    app.listen(port,()=>{
+        console.log(`Server running at port :${port}`);
+    })
+};
+
