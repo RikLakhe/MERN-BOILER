eda276581d85af87ec702b90a201dc30cee7898a
Rikesh Lal Shrestha <rikesh.shrestha@citytech.global>
2020-01-23 16:34:07 +0545

    here working server

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname,"../", "client", "build", "index.html"));
+});
+
+const port = process.env.PORT || "3000";
+
+app.listen(port,()=>{
+    console.log(`Server running at port :${port}`);
+})
