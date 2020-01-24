eb87b40cb1c544526158a6f39f7f247c26fc5bee
Rikesh Lal Shrestha <rikesh.shrestha@citytech.global>
2020-01-24 16:35:58 +0545

    added antd testing

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import {Button} from "antd";
 
 function App() {
   return (
     <div className="App">
       <h1>This is test</h1>
+        <Button >Click me</Button>
     </div>
   );
 }
