1acd0d51362e3e2f7c7da26d090ee0855df34e45
Rikesh Lal Shrestha <rikesh.shrestha@citytech.global>
2020-02-28 17:37:59 +0545

    table all working and dopneeeeeee

--- client/src/components/Common/CustomTable/mainTable.js
+++ client/src/components/Common/CustomTable/mainTable.js
@@ -16,71 +16,49 @@ const useMainTable = props => {
         const [pageArray, setPageArray] = useState([]);
 
         useEffect(() => {
-            if(pagination){
-                console.log('gggg',pagination)
+            if (pagination && pagination.pageNumber && pagination.pageSize) {
                 setPage(pagination.pageNumber);
                 setPageSize(pagination.pageSize);
-                if(pagination.totalRecords % pagination.pageSize > 0){
-                    setPageArray(Array(Math.floor(pagination.totalRecords / pagination.pageSize + 1)))
-                }else{
-                    setPageArray(Array(Math.floor(pagination.totalRecords / pagination.pageSize)))
+                if (pageSize === 'ALL') {
+                    setPageArray(Array())
+                } else {
+                    if (pagination.totalRecords % pagination.pageSize > 0) {
+                        setPageArray(Array(Math.floor(pagination.totalRecords / pagination.pageSize + 1)))
+                    } else {
+                        setPageArray(Array(Math.floor(pagination.totalRecords / pagination.pageSize)))
+                    }
                 }
             }
         }, [pagination]);
 
-        // column && column.map((columnData, columnIndex) => {
-        //     data && Object.values(data).map((columnDataIn, columnIndexIn) => {
-        //         console.log('here', index, column[columnIndex]['dataIndex'] ? column[columnIndex]['dataIndex'] : column[columnIndex]?.render())
-        //         dataSource && dataSource.map((dataSourceData, dataSourceIndex) => {
-        //             data && Object.values(data).map((dataIn, indexIn) => {
-        //
-        //         })
-        //     })
-        // });
-
-        // dataSource && dataSource.map((dataSourceData, dataSourceIndex) => {
-        //     column && column.map((columnData, columnIndex) => {
-        //         let here = column[columnIndex]['dataIndex'];
-        //         console.log('walaaaa', here);
-        //         if (here) {
-        //             if (dataSourceData[here]) {
-        //                 console.log('here', here, dataSourceData[here]);
-        //
-        //             } else {
-        //                 console.log('here else', here, null);
-        //
-        //             }
-        //         } else {
-        //             console.log('here2', here, column[columnIndex]);
-        //             // console.log('here2',column[columnIndex]?.render());
-        //         }
-        //     })
-        // });
-
         const handlePrevious = () => {
             setPage(page - 1);
-            handleTableChange({pageNumber: page, pageSize: pageSize})
+            handleTableChange({pageNumber: page - 1, pageSize: pageSize})
         };
 
         const handleNext = () => {
             setPage(page + 1);
-            handleTableChange({pageNumber: page, pageSize: pageSize})
+            handleTableChange({pageNumber: page + 1, pageSize: pageSize})
         };
 
         const handleSizeChange = (option) => {
             setPageSize(option);
-            handleTableChange({pageNumber: page, pageSize: pageSize})
+            handleTableChange({pageNumber: page, pageSize: option})
+        };
+
+        const handlePageChange = (pageChoice) => {
+            setPageSize(pageChoice);
+            handleTableChange({pageNumber: pageChoice, pageSize: pageSize})
         };
 
         return (
             <div className={'custom-table'}>
                 <table className={'custom-table'}>
                     <thead>
-                    {column && column.map((data, index) => {
-                        return (
+                    {column && column.map((data, index) => (
                             <th key={index}>{data.title}</th>
                         )
-                    })}
+                    )}
                     </thead>
                     <tbody>
                     {
@@ -108,36 +86,43 @@ const useMainTable = props => {
                     }
                     </tbody>
                 </table>
-                {
-                    pageArray && pageArray.length > 0 &&
-                    <div className="table-pagination">
-                        {page !== 1 &&
-                        <button className={`custom-table-pagination-icon`} onClick={handlePrevious}>
-                            <svg viewBox="64 64 896 896" data-icon="left"
-                                 width="1.6em"
-                                 height="1.6em" fill="currentColor" aria-hidden="true">
-                                <path
-                                    d="M724 218.3V141c0-6.7-7.7-10.4-12.9-6.3L260.3 486.8a31.86 31.86 0 0 0 0 50.3l450.8 352.1c5.3 4.1 12.9.4 12.9-6.3v-77.3c0-4.9-2.3-9.6-6.1-12.6l-360-281 360-281.1c3.8-3 6.1-7.7 6.1-12.6z"/>
-                            </svg>
-                        </button>}
-                        {
-                            Array
-                                .from(pageArray)
-                                .map((d, i) => <button
-                                    className={`custom-table-pagination-pages ${i + 1 === page ? "custom-table-pagination-pages-selected" : ""}`}
-                                    disabled={i + 1 === page}>{i + 1}</button>)
-                        }
-                        {page !== (pageArray.length) &&
-                        <button className="custom-table-pagination-icon" onClick={handleNext}>
-                            <svg viewBox="64 64 896 896" data-icon="right"
-                                 width="1.6em"
-                                 height="1.6em" fill="currentColor" aria-hidden="true">
-                                <path
-                                    d="M765.7 486.8L314.9 134.7A7.97 7.97 0 0 0 302 141v77.3c0 4.9 2.3 9.6 6.1 12.6l360 281.1-360 281.1c-3.9 3-6.1 7.7-6.1 12.6V883c0 6.7 7.7 10.4 12.9 6.3l450.8-352.1a31.96 31.96 0 0 0 0-50.4z"/>
-                            </svg>
-                        </button>}
-
-
+                <div className="table-pagination">
+                    {
+                        pageArray && pageArray.length > 0 &&
+                        <div>
+                            {page !== 1 &&
+                            <button className={`custom-table-pagination-icon`} onClick={handlePrevious}>
+                                <svg viewBox="64 64 896 896" data-icon="left"
+                                     width="1.6em"
+                                     height="1.6em" fill="currentColor" aria-hidden="true">
+                                    <path
+                                        d="M724 218.3V141c0-6.7-7.7-10.4-12.9-6.3L260.3 486.8a31.86 31.86 0 0 0 0 50.3l450.8 352.1c5.3 4.1 12.9.4 12.9-6.3v-77.3c0-4.9-2.3-9.6-6.1-12.6l-360-281 360-281.1c3.8-3 6.1-7.7 6.1-12.6z"/>
+                                </svg>
+                            </button>}
+                            {
+                                Array
+                                    .from(pageArray)
+                                    .map((d, i) => (
+                                        <button
+                                            className={`custom-table-pagination-pages ${i + 1 === page ? "custom-table-pagination-pages-selected" : ""}`}
+                                            disabled={i + 1 === page}
+                                            onClick={() => handlePageChange(i+1)}>
+                                            {i + 1}
+                                        </button>))
+                            }
+                            {page !== (pageArray.length) &&
+                            <button className="custom-table-pagination-icon" onClick={handleNext}>
+                                <svg viewBox="64 64 896 896" data-icon="right"
+                                     width="1.6em"
+                                     height="1.6em" fill="currentColor" aria-hidden="true">
+                                    <path
+                                        d="M765.7 486.8L314.9 134.7A7.97 7.97 0 0 0 302 141v77.3c0 4.9 2.3 9.6 6.1 12.6l360 281.1-360 281.1c-3.9 3-6.1 7.7-6.1 12.6V883c0 6.7 7.7 10.4 12.9 6.3l450.8-352.1a31.96 31.96 0 0 0 0-50.4z"/>
+                                </svg>
+                            </button>}
+                        </div>
+                    }
+                    {
+                        pagination &&
                         <select className="custom-table-pagination-select" defaultValue={pagination.pageSize}
                                 onChange={(e) => handleSizeChange(e.target.value)}>
                             {
@@ -145,8 +130,8 @@ const useMainTable = props => {
                                     key={pageSizeOptionData} value={pageSizeOptionData}>{pageSizeOptionData}</option>)
                             }
                         </select>
-                    </div>
-                }
+                    }
+                </div>
             </div>
 
         )
--- controller/categoriesController.js
+++ controller/categoriesController.js
@@ -41,42 +41,79 @@ const addCategory = (req, res, next) => {
 // READ LIST
 const listCategory = (req, res, next) => {
     if (res.locals.decryptData) {
-        const {pageNumber, pageSize} = res.locals.decryptData;
-        Category.find()
-            .sort({date: -1})
-            .exec((error, response) => {
-                if (error) {
-                    res.locals.status = 400;
-                    res.locals.encryptData = {
-                        status: 'FAIL',
-                        message: error
-                    };
-                    next();
-                } else {
-                    if (response.length === 0) {
-                        res.locals.status = 200;
+        const {pageNumber, pageSize} = res.locals.decryptData.pageData;
+        if(pageSize && pageSize === 'ALL'){
+            Category.find()
+                .sort({date: -1})
+                .exec((error, response) => {
+                    if (error) {
+                        res.locals.status = 400;
                         res.locals.encryptData = {
-                            status: 'SUCCESS',
-                            data: {}
+                            status: 'FAIL',
+                            message: error
                         };
                         next();
                     } else {
-                        res.locals.status = 200;
-                        res.locals.encryptData = {
-                            status: 'SUCCESS',
-                            data: response.slice(pageSize || 0,pageNumber || 5),
-                            pagination: {
-                                pageNumber: 1,
-                                pageSize: 5,
-                                totalRecords: response.length,
-                                pageSizeOption: [5,10,15, 'ALL']
-                            }
-                        };
-                        next();
+                        if (response.length === 0) {
+                            res.locals.status = 200;
+                            res.locals.encryptData = {
+                                status: 'SUCCESS',
+                                data: {}
+                            };
+                            next();
+                        } else {
+                            res.locals.status = 200;
+                            res.locals.encryptData = {
+                                status: 'SUCCESS',
+                                data: response,
+                                pagination: {
+                                    pageNumber: pageNumber,
+                                    pageSize: pageSize,
+                                    totalRecords: response.length,
+                                    pageSizeOption: [5, 10, 15, 'ALL']
+                                }
+                            };
+                            next();
+                        }
                     }
-                }
-            });
-    }else{
+                });
+        }else{
+            Category.find()
+                .sort({date: -1})
+                .exec((error, response) => {
+                    if (error) {
+                        res.locals.status = 400;
+                        res.locals.encryptData = {
+                            status: 'FAIL',
+                            message: error
+                        };
+                        next();
+                    } else {
+                        if (response.length === 0) {
+                            res.locals.status = 200;
+                            res.locals.encryptData = {
+                                status: 'SUCCESS',
+                                data: {}
+                            };
+                            next();
+                        } else {
+                            res.locals.status = 200;
+                            res.locals.encryptData = {
+                                status: 'SUCCESS',
+                                data: response.slice((pageNumber - 1) * pageSize || 0, pageSize * pageNumber || 5),
+                                pagination: {
+                                    pageNumber: pageNumber,
+                                    pageSize: pageSize,
+                                    totalRecords: response.length,
+                                    pageSizeOption: [5, 10, 15, 'ALL']
+                                }
+                            };
+                            next();
+                        }
+                    }
+                });
+        }
+    } else {
         Category.find()
             .sort({date: -1})
             .exec((error, response) => {
@@ -99,12 +136,12 @@ const listCategory = (req, res, next) => {
                         res.locals.status = 200;
                         res.locals.encryptData = {
                             status: 'SUCCESS',
-                            data: response.slice(0,5),
+                            data: response.slice(0, 5),
                             pagination: {
                                 pageNumber: 1,
                                 pageSize: 5,
                                 totalRecords: response.length,
-                                pageSizeOption: [5,10,15, 'ALL']
+                                pageSizeOption: [5, 10, 15, 'ALL']
                             }
                         };
                         next();
