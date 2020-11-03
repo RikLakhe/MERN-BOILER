57dac7e86f07116997e92bfde624a26fb0fbb835
Rikesh Lal Shrestha <rikesh.shrestha@citytech.global>
2020-11-03 11:20:33 +0545

    testing

--- index.js
+++ index.js
@@ -14,39 +14,39 @@ const Routes = require('./routes/routes');
 const app = express();
 
 const corsOptions = {
-    exposedHeaders: 'XSRF-TOKEN',
+  exposedHeaders: 'XSRF-TOKEN',
 };
 
 app.use(
-    logger('dev'),
-    cors(corsOptions),
-    express.json(),
-    cookieParser(AppConfig.cookieSecret),
-    express.static(path.resolve(__dirname, "client", "build"))
-    // express.static(path.resolve(__dirname, "testST"))
+  logger('dev'),
+  cors(corsOptions),
+  express.json(),
+  cookieParser(AppConfig.cookieSecret),
+  express.static(path.resolve(__dirname, 'client', 'build'))
+  // express.static(path.resolve(__dirname, "testST"))
 );
 app.disable('etag');
 
 // Router
-app.use("/v1", Routes);
+app.use('/v1', Routes);
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-    // res.sendFile(path.join(__dirname, "testST", "index.html"));s
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  // res.sendFile(path.join(__dirname, "testST", "index.html"));s
 });
 
-const port = process.env.PORT || "3001";
+const port = process.env.PORT || '3001';
 
 app.listen(port, () => {
-    console.log(`server running in port : ${port}`);
+  console.log(`server running in port : ${port}`);
 
-    // replace the uri string with your connection string.
-    // mongoose
-    //     .connect(AppConfig.mongoDB.url, {
-    //         dbName: AppConfig.mongoDB.database,
-    //         useNewUrlParser: true,
-    //         useUnifiedTopology: true
-    //     })
-    //     .then(() => console.log(`DB Connected Database : ${AppConfig.mongoDB.database}`))
-    //     .catch(err => console.log('error', err));
+  // replace the uri string with your connection string.
+  // mongoose
+  //     .connect(AppConfig.mongoDB.url, {
+  //         dbName: AppConfig.mongoDB.database,
+  //         useNewUrlParser: true,
+  //         useUnifiedTopology: true
+  //     })
+  //     .then(() => console.log(`DB Connected Database : ${AppConfig.mongoDB.database}`))
+  //     .catch(err => console.log('error', err));
 });
